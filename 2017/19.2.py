6d29cf31b3de4d120eba7c552cfd5dfd7642920f
Alex Jacobs <ajacobs@factset.com>
2017-12-19 00:35:47 -0500

    2017 - Day 19

---
+++ 2017/19.2.py
@@ -0,0 +1,38 @@
+import sys
+
+grid = list(sys.stdin.readlines())
+
+UP = (0, -1)
+DOWN = (0, 1)
+LEFT = (-1, 0)
+RIGHT = (1, 0)
+
+y = 0
+x = grid[0].find("|")
+dx, dy = DOWN
+
+steps = 1
+letters = []
+while True:
+    x += dx
+    y += dy
+    cell = grid[y][x]
+    if cell == " ":
+        break
+    steps += 1
+    if cell == "+":
+        if dx:
+            if grid[y-1][x] == "|":
+                dx, dy = UP
+            else:
+                dx, dy = DOWN
+        else:
+            if grid[y][x-1] == "-":
+                dx, dy = LEFT
+            else:
+                dx, dy = RIGHT
+    elif cell.isalpha():
+        letters.append(cell)
+    # dont care about | and -
+
+print("".join(letters), steps)
