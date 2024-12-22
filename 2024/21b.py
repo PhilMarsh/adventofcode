7845ff909ca05ccefc7b090981f930e654c42f32
PhilMarsh <am_scztp@hotmail.com>
2024-12-22 17:24:21 -0500

    2024, Day 21

---
+++ 2024/21b.py
@@ -0,0 +1,146 @@
+import functools
+
+from vector import Vector
+
+
+_UP = "^"
+_DOWN = "v"
+_LEFT = "<"
+_RIGHT = ">"
+_ACTIVATE = "A"
+
+_ARROWPAD_BLANK_CORNER = Vector(x=0, y=0)
+_ARROWPAD_SYMBOL_POSITIONS = {
+    # blank: Vector(x=0, y=0),
+    _UP: Vector(x=1, y=0),
+    _ACTIVATE: Vector(x=2, y=0),
+    _LEFT: Vector(x=0, y=1),
+    _DOWN: Vector(x=1, y=1),
+    _RIGHT: Vector(x=2, y=1),
+}
+
+_NUMPAD_BLANK_CORNER = Vector(x=0, y=3)
+_NUMPAD_SYMBOL_POSITIONS = {
+    "7": Vector(x=0, y=0),
+    "8": Vector(x=1, y=0),
+    "9": Vector(x=2, y=0),
+    "4": Vector(x=0, y=1),
+    "5": Vector(x=1, y=1),
+    "6": Vector(x=2, y=1),
+    "1": Vector(x=0, y=2),
+    "2": Vector(x=1, y=2),
+    "3": Vector(x=2, y=2),
+    # blank: Vector(x=0, y=3),
+    "0": Vector(x=1, y=3),
+    _ACTIVATE: Vector(x=2, y=3),
+}
+
+
+def main():
+    numpad_codes = _load_input()
+
+    arrowpad_command_lens = [
+        _arrowpad_command_len_for_numpad_code(code, target_arrowpad_depth=25)
+        for code in numpad_codes
+    ]
+
+    complexity_scores = [
+        cmd * _numpad_code_int(code)
+        for cmd, code in zip(arrowpad_command_lens, numpad_codes)
+    ]
+
+    res = sum(complexity_scores)
+    print(res)
+
+
+def _load_input():
+    with open("21.in") as file:
+        return [x.strip() for x in file.readlines()]
+
+
+def _arrowpad_command_len_for_numpad_code(code, target_arrowpad_depth):
+    return sum(
+        _best_numpad_move_len(start, end, target_arrowpad_depth)
+        for start, end in zip(f"A{code}", code)
+    )
+
+
+def _arrowpad_command_len_for_arrowpad_command(command, target_depth, depth=0):
+    if depth == target_depth:
+        return len(command)
+
+    return sum(
+        _best_arrowpad_move_len(start, end, target_depth, depth + 1)
+        for start, end in zip(f"A{command}", command)
+    )
+
+
+@functools.cache
+def _best_numpad_move_len(start_symbol, end_symbol, target_arrowpad_depth):
+    base_arrowpad_options = _numpad_move_options(start_symbol, end_symbol)
+    return min(
+        _arrowpad_command_len_for_arrowpad_command(cmd, target_arrowpad_depth)
+        for cmd in base_arrowpad_options
+    )
+
+
+@functools.cache
+def _best_arrowpad_move_len(start_symbol, end_symbol, target_depth, depth):
+    base_arrowpad_options = _arrowpad_move_options(start_symbol, end_symbol)
+    return min(
+        _arrowpad_command_len_for_arrowpad_command(cmd, target_depth, depth)
+        for cmd in base_arrowpad_options
+    )
+
+
+def _numpad_move_options(start_symbol, end_symbol):
+    start_pos = _NUMPAD_SYMBOL_POSITIONS[start_symbol]
+    end_pos = _NUMPAD_SYMBOL_POSITIONS[end_symbol]
+    return _pad_move_options_avoid_corner(start_pos, end_pos, _NUMPAD_BLANK_CORNER)
+
+
+def _arrowpad_move_options(start_symbol, end_symbol):
+    start_pos = _ARROWPAD_SYMBOL_POSITIONS[start_symbol]
+    end_pos = _ARROWPAD_SYMBOL_POSITIONS[end_symbol]
+    return _pad_move_options_avoid_corner(start_pos, end_pos, _ARROWPAD_BLANK_CORNER)
+
+
+def _pad_move_options_avoid_corner(start_pos, end_pos, bad_corner_pos):
+    delta = end_pos - start_pos
+    moves = ""
+
+    if delta.x > 0:
+        h_moves = _RIGHT * delta.x
+    elif delta.x < 0:
+        h_moves = _LEFT * -delta.x
+    else:
+        h_moves = ""
+
+    if delta.y < 0:
+        v_moves = _UP * -delta.y
+    elif delta.y > 0:
+        v_moves = _DOWN * delta.y
+    else:
+        v_moves = ""
+
+    if not h_moves:
+        move_options = (v_moves,)
+    elif not v_moves:
+        move_options = (h_moves,)
+    else:
+        move_options = tuple()
+        if start_pos.x != bad_corner_pos.x or end_pos.y != bad_corner_pos.y:
+            move_options += (v_moves + h_moves,)
+
+        if end_pos.x != bad_corner_pos.x or start_pos.y != bad_corner_pos.y:
+            move_options += (h_moves + v_moves,)
+
+    return [f"{opt}A" for opt in move_options]
+
+
+def _numpad_code_int(code):
+    return int(code[:-1])
+
+
+if __name__ == "__main__":
+    main()
