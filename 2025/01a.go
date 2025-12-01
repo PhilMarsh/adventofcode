a9ab06d6690639db73f798cc45cd003fd4e36223
Phil Marsh <philmarsh.github@gmail.com>
2025-12-01 11:29:04 -0500

    2025 - Day 1 (golang)

---
+++ 2025/01a.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const DIAL_START = 50
+const DIAL_SIZE = 100
+
+func main() {
+	file, _ := os.Open("01.in")
+	scanner := bufio.NewScanner(file)
+
+	position := DIAL_START
+	zeros := 0
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+		rotation := _parse_rotation(line)
+		position = (position + (rotation.direction * rotation.distance)) % DIAL_SIZE
+		if position == 0 {
+			zeros += 1
+		}
+	}
+	fmt.Printf("%d\n", zeros)
+}
+
+type Rotation struct {
+	direction int
+	distance  int
+}
+
+func _parse_rotation(line string) Rotation {
+	distance_str, is_left := strings.CutPrefix(line, "L")
+	var direction int
+	if is_left {
+		direction = -1
+	} else {
+		distance_str, _ = strings.CutPrefix(line, "R")
+		direction = 1
+	}
+
+	distance, _ := strconv.Atoi(distance_str)
+	return Rotation{
+		direction: direction,
+		distance:  distance,
+	}
+
+}
